b3ac418c6935e8d91ec401b46699abb37c5ef99b
Fahmi Ilmawan <fahmikeilmawan22@gmail.com>
2025-02-02 20:08:14 +0700

    Just push

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::get('/kader-adiwiyata', function () {
 })->name('kader');
 
 Route::get('/print-laporan-pemasukan',[LaporanPemasukanController::class, 'printPDF'])->name('print.laporan-pemasukan');
+
 Route::get('/print-qr-code',[TanamanController::class, 'printQRCode'])->name('print.qr-code');
